d138995712c0f8ece37b3baf859327391a0c50c5
oumaima <oumaimanaanaapro@gmail.com>
2024-06-12 17:39:58 +0100

    comment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+-- creates a table users
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
 CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
