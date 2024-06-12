f686d8c3cb4d4bee4d7df8e8f1f07f8549db14b6
oumaima <oumaimanaanaapro@gmail.com>
2024-06-12 17:56:01 +0100

    1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- creates a table users
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
