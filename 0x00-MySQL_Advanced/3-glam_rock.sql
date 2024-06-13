4e2809fd9a04d6983ddae41fd96425c38e7bea05
oumaima <oumaimanaanaapro@gmail.com>
2024-06-14 00:09:55 +0100

    3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,2 @@
+-- script that lists all bands with Glam rock as their main style, ranked by their longevity
+select band_name, (IFNULL(split, 2022) - formed) as lifespan from metal_bands where style like  "%Glam rock%" order by lifespan DESC;
