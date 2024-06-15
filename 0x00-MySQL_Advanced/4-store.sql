3be8ad1504abcecaa9d62894567d486040954880
oumaima <oumaimanaanaapro@gmail.com>
2024-06-15 23:32:04 +0100

    4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- creates a trigger that decreases the quantity of an item after adding a new order.
+
+UPDATE items SET quantity = (
+	SELECT SUM(orders.number)
+	FROM orders
+	WHERE orders.item_name=items.name
+)
+WHERE items.name IN (SELECT item_name FROM orders);
