44d3f6c766e8351987018cc701e562aedb82db9e
oumaima <oumaimanaanaapro@gmail.com>
2024-06-16 14:27:50 +0100

    6 comment

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,3 +1,4 @@
+-- creates a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER $$
 USE holberton $$
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
