fb982442008a1ebfd167c821f0812eac0c2fa945
oumaima <oumaimanaanaapro@gmail.com>
2024-06-16 14:33:13 +0100

    remove database name t6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,9 +1,7 @@
 -- creates a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER $$
-USE holberton $$
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-	DECLARE project_exists BOOLEAN;
 	DECLARE project_id INT;
 
 	SELECT id
@@ -12,9 +10,8 @@ BEGIN
 	WHERE name = project_name
 	LIMIT 1;
 	
-    SET project_exists = (project_id IS NOT NULL);
     
-	IF NOT project_exists THEN
+	IF project_id IS NULL THEN
 		INSERT INTO projects (name)
 		VALUES (project_name);
         SET project_id = LAST_INSERT_ID();
