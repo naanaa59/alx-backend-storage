d8231bf2d4726c2bd12de5c589310aa73b140f1f
oumaima <oumaimanaanaapro@gmail.com>
2024-06-19 20:39:15 +0100

    0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Class Cache """
+
+
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """ Class Cache """
+
+    def __init__(self) -> None:
+        """ init method """
+        self._redis = redis.Redis(host='localhost', db=0)
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ store method """
+        key = str(uuid.uuid1())
+        self._redis.set(key, data)
+        return key
