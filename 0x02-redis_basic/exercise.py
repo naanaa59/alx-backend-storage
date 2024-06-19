e21d07514cda3a5d1e7414f6f6d256572406ae72
oumaima <oumaimanaanaapro@gmail.com>
2024-06-20 00:49:32 +0100

    task4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,56 @@
 import redis
 from typing import Union, Any, Callable, Optional
 import uuid
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """doc doc class"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """doc doc class"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """doc doc class"""
+    inkey = method.__qualname__ + ":inputs"
+    outkey = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """doc doc class"""
+        self._redis.rpush(inkey, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(outkey, str(res))
+        return res
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    # sourcery skip: use-fstring-for-concatenation, use-fstring-for-formatting
+    """
+    Replays the history of a function
+    Args:
+        method: The function to be decorated
+    Returns:
+        None
+    """
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
 
 
 class Cache:
@@ -14,14 +64,17 @@ class Cache:
         """ init method """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
+    
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store method """
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """ get a key and convert it to a desired format"""
         value = self._redis.get(key)
         if fn is not None:
@@ -30,8 +83,14 @@ class Cache:
 
     def get_str(self, key: str) -> str:
         """ gets a value and convert it to a string"""
-        return self.get(key, fn=str)
+        value = self._redis.get(key)
+        return value.decode('utf-8')
 
     def get_int(self, key: str) -> int:
         """ gets a value and convert it to a string"""
-        return self.get(key, fn=int)
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
