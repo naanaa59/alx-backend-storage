41a147062098a5d9d9bda08204509e2da0f4a4b7
oumaima <oumaimanaanaapro@gmail.com>
2024-06-19 01:32:36 +0100

    101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """ returns all students sorted by average score"""
+    all_students = mongo_collection.find()
+    total = 0
+
+    for student in all_students:
+        total = 0
+        for topic in student["topics"]:
+            total += topic["score"]
+        av_score = total / 3
+        mongo_collection.update_one({
+            "name": student["name"]}, {'$set': {"averageScore": av_score}})
+
+    all_students = mongo_collection.find()
+    return sorted(
+            list(all_students), key=lambda x: x['averageScore'], reverse=True)
